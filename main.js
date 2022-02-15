300e3b1df93d7acf01ca6eb5501fdc945fdcf9d0
konstantinosTabakis <konstantinos_tabakis@yahoo.com>
2022-02-15 22:25:46 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+let currentPage = 1;
+
+function movePage(e, page) {
+  if (page == currentPage) {
+    currentPage+=2;
+    e.classList.toggle('left-side')
+    e.nextElementSibling.classList.toggle('left-side')
+    
+    
+  }
+  else if (page = currentPage - 1) {
+    currentPage-=2;
+    e.classList.toggle('left-side')
+    e.previousElementSibling.classList.toggle('left-side')
+    
+  }
+  
+}
+
+// fetch('https://api.quotable.io/random')
+// .then(res=> res.json())
+// .then(data =>{
+//   console.log(data);
+//   document.querySelector('.page2 .quote').innerHTML= data.content
+//   document.querySelector('.page2 .author').innerHTML= data.author
+// })
